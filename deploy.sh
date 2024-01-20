95c2374545db077436587eb97b59db59d119cdaa
Scott Bisese <bisese.scott@Gmail.com>
2024-01-19 19:12:41 -0600

    fixed deployment script

--- deploy.sh
+++ deploy.sh
@@ -1,7 +1,13 @@
 #!/bin/bash
-# Move to the repository root
-cd "$DEPLOYMENT_SOURCE"
+echo "Starting deployment script."
 
+# Paths
+SOURCE_DIR="$DEPLOYMENT_SOURCE" # The source directory of your project
+TARGET_DIR="$DEPLOYMENT_TARGET/wwwroot" # The Azure wwwroot directory
+
+echo "Copying files from $SOURCE_DIR to $TARGET_DIR"
 # Copy files to wwwroot
-cp -R . "$DEPLOYMENT_TARGET"
+cp -R $SOURCE_DIR/* $TARGET_DIR
+
+echo "Deployment script finished."
 
